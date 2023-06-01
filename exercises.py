057908949737194158de48d3edc8a4361488bc0a
Nicolas <nikolasdurango@gmail.com>
2023-05-31 20:05:40 -0500

    file duplicated ?

--- exercises.py
+++ exercises.py
@@ -12,3 +12,20 @@ def fizz_buzz():
             print(i)
 
 fizz_buzz()
+
+### ANAGRAMS ###
+
+word1_test_a = ("Hello")
+word2_test_a = ("One")
+
+word1_test_b = ("State")
+word2_test_b = ("Taste")
+
+def anagrams(word1, word2):
+    if sorted(word2.lower()) == sorted(word1.lower()):
+        return True
+    else:
+        return False
+
+print(anagrams(word1_test_a, word2_test_a))
+print(anagrams(word1_test_b, word2_test_b))
