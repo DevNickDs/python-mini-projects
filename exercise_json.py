bad363a947296ac7986c1284554f431e9761fb27
Nicolas <nikolasdurango@gmail.com>
2023-06-05 15:25:06 -0500

    rewrite the json file with python and their module json

--- exercise_json.py
+++ exercise_json.py
@@ -5,8 +5,15 @@ json_file = open("python_mini_projects\my_json.json", "w+")
 json_test = {
     "name": "NiubProgrammer",
     "age": 21,
-    "language": "Python",
+    "language": ["Python", "JavaScript", "C++"],
     "is_cool": True
 }
 
-json.dump(json_test, json_file)
+json.dump(json_test, json_file, indent = 4)
+
+json_file.close()
+
+with open("python_mini_projects\my_json.json", "r") as my_other_json:
+    for i in my_other_json.readlines():
+        print(i)
+
